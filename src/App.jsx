e61e472ec7ada77b12021f74b0286c3c4406c265
OV1 Kenobi <ov1_kenobi@mailfence.com>
2025-05-29 17:00:31 -0400

    Add dedicated Citadel Academy theme CSS file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
+import './citadel-theme.css';
 import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 
